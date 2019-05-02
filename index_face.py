64fbde5e518baf6a66d584dcc4ecb3bdd7fe47df
Neeraj Roy <Neerajdel98@gmail.com>
2019-05-02 18:07:30 +0530

    Update index_face.py
    [x]Added Comments
    [x] Removed active tokens

--- index_face.py
+++ index_face.py
@@ -2,19 +2,20 @@ import sys
 import boto3
 client = boto3.client("rekognition")
 def indexFace():
+	'''index_faces indexes stored faces in an image to a collection that has already been created'''
 	response = client.index_faces(
-	    CollectionId='raspox3',
+	    CollectionId='put_anything_here', #refer to createcollection.py
 	    Image={
 	        'S3Object': {
-	            'Bucket': 'raspo',
+	            'Bucket': 'put_anything_here_2', #refer to createnewbucket.py
 	            'Name': str(sys.argv[1])
 	        }
 	    },
-            MaxFaces = 1,
-	    ExternalImageId=str(sys.argv[2]),
+            MaxFaces = 1, #decides the no of faces to index;in this case it will only index the clearest face
+	    ExternalImageId=str(sys.argv[2]), #indexes the image with an externalID
 	    DetectionAttributes=[
 	        'DEFAULT','ALL'
 	    ]
             )
-print(str(sys.argv))	    
+#print(str(sys.argv))	    
 indexFace()
