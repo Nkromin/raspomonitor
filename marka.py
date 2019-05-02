c48fe0fdd943375539cabe5fb54a7fc2b146ffee
Neeraj Roy <Neerajdel98@gmail.com>
2019-05-02 18:10:39 +0530

    Update marka.py
    [x]Added Comments
    [x]Removed Active Tokens

--- marka.py
+++ marka.py
@@ -1,8 +1,9 @@
+'''The Driver code to execute the search_in_thecollection.py file'''
 import os
 s = "test"
 s1 = s+'.jpg'
-os.system("raspistill -o "+s1+" -t 3000")
-print("Identifying blob within collection")
+os.system("raspistill -o "+s1+" -t 3000") #Taking Single Image from RaspberryPI Camera
+print("Identifying face within collection")
 os.system("python3 search_index_face.py "+s1)
 
 
