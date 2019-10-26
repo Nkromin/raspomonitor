4d74dc52f9e728fda9f237f8cbb8cfa02b1b608f
Neeraj Roy <Neerajdel98@gmail.com>
2019-10-27 03:18:54 +0530

    Creates new S3 bucket

--- createnewbucker.py
+++ createnewbucker.py
@@ -1,6 +1,6 @@
 import boto3
-s3 = boto3.resource('s3') #s3 is the service to store the images of the individual on cloud
-'''ACL is the key to remotely accessing the files on the cloud'''
+s3 = boto3.resource('s3') #s3 is the service to store the images of the individual on cloud 
+'''ACL is the key to remotely accessing the files on the cloud ok'''
 bucket = s3.create_bucket( 
     ACL='public-read-write',
     Bucket='put_anything_here'
