d87f1e8382e1390439d990b82693f70fc228f68c
Neeraj Roy <Neerajdel98@gmail.com>
2019-10-27 03:16:46 +0530

    Creates an Collection for AWS rekog to work on
    CreateCollection Operation Request
    
    The input to CreationCollection is the name of the collection that you want to create.
    
    {
        "CollectionId": "MyCollection"
    }
    
    CreateCollection Operation Response
    
    Amazon Rekognition creates the collection and returns the Amazon Resource Name (ARN) of the newly created collection.
    
    {
       "CollectionArn": "aws:rekognition:us-east-1:acct-id:collection/examplecollection",
       "StatusCode": 200
    }

--- cratenewcollection.py
+++ cratenewcollection.py
@@ -1,5 +1,5 @@
 import boto3
-'''This Collection is used later by aws rekog to index faces within'''
+'''This Collection is used later by aws rekog to inde'''
 COLLECTION = 'put_anything_here'
 rekog = boto3.client("rekognition")  #rekognition is the service we would use for facial identification
 respo = rekog.create_collection(CollectionId=COLLECTION)
